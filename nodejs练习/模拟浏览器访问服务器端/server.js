79f86bcc0c041d02aa74753575439ee789926258
BUILDY0 <buildy@qq.com>
2021-10-05 23:52:38 +0800

    10-5

---
+++ nodejs练习/模拟浏览器访问服务器端/server.js
@@ -0,0 +1,17 @@
+const {
+    Socket
+} = require('dgram');
+const net = require('net');
+const server = net.createServer();
+server.listen(80, '127.0.0.1')
+
+server.on("listening", () => {
+    console.log('服务器已连接！');
+})
+
+server.on('connection', (socket) => {
+    socket.on('data', (data) => {
+        console.log(data.toString());
+        socket.write('HTTP 200OK\r\nContent-type:text/html\r\nServer:hello/1.1\r\n\r\n<html><body>hello broswer!</body></html>')
+    })
+})
