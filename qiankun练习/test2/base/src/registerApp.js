6201e5b2d520607a5b1559c007699c57ce4a2a40
BUILDY0 <buildy@qq.com>
2021-11-02 14:02:05 +0800

    11-2

--- qiankun练习/test2/base/src/registerApp.js
+++ qiankun练习/test2/base/src/registerApp.js
@@ -33,4 +33,14 @@ registerMicroApps([{
     }
 })
 
-start();
+start({
+    /**
+     * 样式隔离可以采用两种方式：（二选一）
+     * 一、开启sandbox选项，这样会用CSSmodules的方式解决，为每个微应用加上data-qiankun="%name%"的自定义行间属性，并在样式表中使用属性选择器选出dom并添加样式（推荐）
+     * 二、使用shadowDOM的方法，缺点是增加全局样式会出现问题。
+     */
+    sandbox:{
+        experimentalStyleIsolation:true,//CSSmodules方式
+        // strictStyleIsolation:true,//shadowDOM方式
+    }
+});
