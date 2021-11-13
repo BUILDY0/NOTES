b92f022c2ab3121770eddf92aee7f8ca82fd7cdd
BUILDY0 <buildy@qq.com>
2021-11-13 20:45:28 +0800

    11-13

---
+++ echarts练习/test2/src/App.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div id="app">
+    <v-chart :option='data' class='charts'/>
+  </div>
+</template>
+
+<script>
+import 'echarts';
+export default {
+  name: 'App',
+  data(){
+    return {
+      data:{
+        tiele:{
+          text:'hello',
+        },
+        xAxis:{
+          data:'ABCDE'.split('')
+        },
+        yAxis:{
+
+        },
+        series:{
+          name:'value',
+          type:'bar',
+          data:[5,10,15,20,25]
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chart{
+  height: 800px;
+  width: 100%;
+  border: 1px solid yellow;
+}
+</style>
+
+
